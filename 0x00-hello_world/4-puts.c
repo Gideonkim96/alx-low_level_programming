3a890dcce749b68670628f64af00092faf5a6f75
Gideonkim96 <ronogideon97@gmail.com>
2022-09-08 13:53:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,11 @@
  *puts(): prints to stdout
  * Return: Always 0 (Success)
  */
+
 int main(void)
 { 
        	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+
+	return (0);
 }
 
