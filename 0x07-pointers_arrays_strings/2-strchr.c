8ffea17819e8fbc353b2bccbe2587bd4bd3b209c
Gideonkim96 <ronogideon97@gmail.com>
2022-09-26 11:30:15 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strchr - function that locates a character in a string.
+ * _strchr- locates character in a string
+ * @s: string to check
+ * @c: character to check for
  *
- * @s: string to receive and return
- * @c: character to receive
- * Return: char
+ * Return: pointer to spot in s with c or null
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; *(s + i); i++)
 	{
-	if (*s == c)
-		return (s);
-		s++;
+	if (*(s + i) == c)
+											return (s + i);
 	}
-	if (*s == c)
-	return (s);
-	else
-	return (NULL);
+	if (*(s + i) == c)
+	return (s + i);
+	return (0);
 }
