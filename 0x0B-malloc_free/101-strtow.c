9d2d0b23920bd3a299322dca4c6614f709c2a763
Gideonkim96 <ronogideon97@gmail.com>
2022-10-04 12:06:47 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,90 +1,147 @@
 #include "main.h"
+#include <stdlib.h>
 
-int word_len(char *str);
-int count_words(char *str);
-char **strtow(char *str);
 /**
- * word_len - Locates the index marking the end of the
- *            first word contained within a string.
- * @str: The string to be searched.
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
  *
- * Return: The index marking the end of the initial word pointed to by str.
+ * Return: void
  */
-int word_len(char *str)
+void _print(char *str, int l)
 {
-	int index = 0, len = 0;
+	int i, j;
 
-	while (*(str + index) && *(str + index) != ' ')
+	i = j = 0;
+	while (i < l)
 	{
-		len++;
-		index++;
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
 	}
 
-	return (len);
+	_putchar('\n');
+	free(str);
 }
 
 /**
- * count_words - Counts the number of words contained within a string.
- * @str: The string to be searched.
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
  *
- * Return: The number of words contained within str.
+ * Return: pointer to dest, or NULL on failure
  */
-int count_words(char *str)
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
-	int index = 0, words = 0, len = 0;
+	int j, k, mul, mulrem, add, addrem;
 
-	for (index = 0; *(str + index); index++)
-		len++;
-
-	for (index = 0; index < len; index++)
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
 	{
-		if (*(str + index) != ' ')
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
+}
+/**
+ * check_for_digits - checks the arguments to ensure they are digits
+ * @av: pointer to arguments
+ *
+ * Return: 0 if digits, 1 if not
+ */
+int check_for_digits(char **av)
+{
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; av[i][j]; j++)
 		{
-			words++;
-			index += word_len(str + index);
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (1);
 		}
 	}
-
-	return (words);
+	return (0);
 }
 
 /**
- * strtow - Splits a string into words.
- * @str: The string to be split.
+ * init - initializes a string
+ * @str: sting to initialize
+ * @l: length of strinf
  *
- * Return: If str = NULL, str = "", or the function fails - NULL.
- *         Otherwise - a pointer to an array of strings (words).
+ * Return: void
  */
-char **strtow(char *str)
+void init(char *str, int l)
 {
-	char **strings;
-	int index = 0, words, w, letters, l;
+	int i;
 
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
+	for (i = 0; i < l; i++)
+		str[i] = '0';
+	str[i] = '\0';
+}
 
-	words = count_words(str);
-	if (words == 0)
-		return (NULL);
+/**
+ * main - multiply two numbers
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: zero, or exit status of 98 if failure
+ */
+int main(int argc, char *argv[])
+{
+	int l1, l2, ln, ti, i;
+	char *a;
+	char *t;
+	char e[] = "Error\n";
 
-	strings = malloc(sizeof(char *) * (words + 1));
-	if (strings == NULL)
-		return (NULL);
-
-	for (w = 0; w < words; w++)
+	if (argc != 3 || check_for_digits(argv))
 	{
-		while (str[index] == ' ')
-			index++;
-
-		letters = word_len(str + index);
-
-		strings[w] = malloc(sizeof(char) * (letters + 1));
-
-		if (strings[w] == NULL)
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	a = malloc(ln * sizeof(char));
+	if (a == NULL)
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		if (t == NULL)
 		{
-			for (; w >= 0; w--)
-				free(strings[w]);
-
-			free(strings);
-			return (NULL);
+			for (ti = 0; e[ti]; ti++)
+				_putchar(e[ti]);
+			free(a);
+			exit(98);
 		}
+	}
+	_print(a, ln - 1);
+	return (0);
+}
