44d0b736602616f04d0560abbf18cfbd1a693141
Gideonkim96 <ronogideon97@gmail.com>
2022-10-06 12:46:26 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,42 +1,18 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string to copy
- * @s2: second string to copy
- * @n: number of bytes of s2 to copy
- *
- * Return: char pointer to newly allocated place in memory
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	unsigned int i, j, k;
-	char *s;
+  *malloc_checked - check if malloc library is assigning memories
+  *@b: variable
+  *Return: integer
+  */
 
-	if (s1 == NULL)
-		i = 0;
-	else
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
 	{
-		for (i = 0; s1[i]; i++)
-			;
+	exit(98);
 	}
-	if (s2 == NULL)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-			;
-	}
-	if (j > n)
-		j = n;
-	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
-		return (NULL);
-	for (k = 0; k < i; k++)
-		s[k] = s1[k];
-	for (k = 0; k < j; k++)
-		s[k + i] = s2[k];
-	s[i + j] = '\0';
-	return (s);
+	return (ptr);
 }
